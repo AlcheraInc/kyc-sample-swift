4ecd2bf9e489793515d6fac6a9b450b36e841ba2
yurimrim <yr.kim@alcherainc.com>
2022-03-30 17:24:45 +0900

    feat: postMessage sample

---
+++ EkycSample/KycResponse.swift
@@ -0,0 +1,52 @@
+//
+//  KycResponse.swift
+//  EkycSample
+//
+//  Created by Alchera on 2022/03/30.
+//
+
+import Foundation
+
+struct KycResponse: Codable {
+    let result: String
+    let review_result: Review_result?
+}
+
+struct Review_result: Codable {
+    let name: String
+    let phone_number: String
+    let birthday: String
+    let module: Module
+    let id_card: Id_card
+    let face_check: Face_check?
+    let account: Account?
+}
+
+struct Module: Codable {
+    let id_card_ocr: Bool
+    let id_card_verification: Bool
+    let face_authentication: Bool
+    let account_verification: Bool
+    let liveness: Bool
+}
+
+struct Id_card: Codable {
+    let verified: Bool
+    let id_card_image: Data
+    let id_card_origin: Data
+    let id_crop_image: Data
+}
+
+struct Face_check: Codable {
+    let is_same_person: Bool
+    let is_live: Bool
+    let selfie_image: Data
+}
+
+struct Account: Codable {
+    let verified: Bool
+    let user_name: String?
+    let finance_company: String?
+    let finance_code: String?
+    let account_number: String?
+}
---
+++ EkycSample/WebViewController.swift
@@ -0,0 +1,148 @@
+//
+//  WebViewController.swift
+//  EkycSample
+//
+//  Created by Alchera on 2022/03/21.
+//
+
+import UIKit
+import WebKit
+import AVFoundation
+
+class WebViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
+    static let storyboardID = "webView"
+    
+    var webView: WKWebView!
+    private let responseName = "alcherakyc"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "eKYC"
+        
+        checkCameraPermission()
+    }
+    
+    /* View 불러오기 */
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.allowsInlineMediaPlayback = true
+        // 고객 정보를 담은 postMessage 설정
+        if let requestData = encodedPostMessage() {
+            let userScript = WKUserScript(source: "postMessage('\(requestData)')", injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+            webConfiguration.userContentController.addUserScript(userScript)
+            webConfiguration.preferences.javaScriptEnabled = true
+        }
+        // 메시지 수신할 핸들러 등록
+        webConfiguration.userContentController.add(self, name: responseName)
+        
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    /* WebView 불러오기 */
+    func loadWebView() {
+        //        guard let url = URL(string: "https://kyc.useb.co.kr/auth") else { return }
+        guard let url = URL(string: "https://kyc-int.useb.co.kr/auth") else { return }
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+    }
+    
+    /* WebView 메시지 핸들러 */
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard message.name == responseName, let body = message.body as? String else { return }
+        guard let kycResponse = decodedPostMessage(body) else {
+            NSLog("KYC 응답 메시지 분석에 실패했습니다.")
+            return
+        }
+        
+        NSLog("KYC 응답 메시지 = \(kycResponse)")
+        
+        switch kycResponse.result {
+        case "success":
+            NSLog("KYC 작업이 성공했습니다.")
+        case "failed":
+            NSLog("KYC가 작업이 실패했습니다.")
+        case "complete":
+            NSLog("KYC가 완료되었습니다.")
+            navigationController?.popViewController(animated: true)
+        case "close":
+            NSLog("KYC가 완료되지 않았습니다.")
+            navigationController?.popViewController(animated: true)
+        default:
+            break
+        }
+    }
+    
+    /* Camera 권한 체크 */
+    func checkCameraPermission() {
+        let permission = AVCaptureDevice.authorizationStatus(for: .video)
+        switch permission {
+        case .authorized:
+            DispatchQueue.main.async { self.loadWebView() }
+        case .denied:
+            let alert = UIAlertController(title: "카메라 권한 필요",
+                                          message: "설정 > 개인 정보 보호 > 카메라에서 권한을 변경하실 수 있습니다.",
+                                          preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: false, completion: nil)
+            return
+        case .notDetermined:
+            // 권한 요청
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    DispatchQueue.main.async { self.loadWebView() }
+                } else {
+                    NSLog("권한이 거부되었습니다.")
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+        default:
+            NSLog("Permission = \(permission.rawValue)")
+        }
+    }
+    
+    /* PostMessage로 보낼 고객 정보를 생성합니다. */
+    func encodedPostMessage() -> String? {
+        let jsonData: [String: Any] = [
+            "customer_id": "12",
+            "id": "demoUser",
+            "key": "demoUser0000!",
+            "name": "김유림",
+            "birthday": "1993-05-12",
+            "phone_number": "01057652566",
+            "email": "email@mail.com"
+        ] as Dictionary
+        
+        do {
+            // JSON -> encodeURIComponent -> Base64Encoding
+            let jsonData = try JSONSerialization.data(withJSONObject: jsonData, options: .prettyPrinted)
+            if let jsonString = String(data: jsonData, encoding: .utf8),
+               let uriEncoded = jsonString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                return uriEncoded.data(using: .utf8)?.base64EncodedString()
+            }
+        } catch {
+            NSLog(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+    /* KYC 수행 결과를 분석합니다. */
+    func decodedPostMessage(_ encodedMessage: String) -> KycResponse? {
+        // Base64Decoding -> decodeURIComponent -> JSON
+        if let base64DecodedData = Data(base64Encoded: encodedMessage),
+           let base64DecodedString = String(data: base64DecodedData, encoding: .utf8),
+           let uriDecodedString = base64DecodedString.removingPercentEncoding {
+            if let uriDecodedData = uriDecodedString.data(using: .utf8) {
+                let decoder = JSONDecoder()
+                let response = try? decoder.decode(KycResponse.self, from: uriDecodedData)
+                return response
+            }
+        }
+        
+        return nil
+    }
+}
